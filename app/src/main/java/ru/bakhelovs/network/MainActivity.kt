527270f641e51b04e881fe244fb94d683904bdc6
BakhelovS <sespace@ya.ru>
2021-05-13 18:50:04 +0300

    Turned off NightMode

--- app/src/main/java/ru/bakhelovs/network/MainActivity.kt
+++ app/src/main/java/ru/bakhelovs/network/MainActivity.kt
@@ -2,11 +2,13 @@ package ru.bakhelovs.network
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import ru.bakhelovs.network.ui.main.BreedFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
